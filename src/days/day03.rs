cb2b675cde192ca965058f82f6fcd925cea3e40f
Scott Semyan <scottse@microsoft.com>
2022-12-07 11:24:19 -0800

    Day 3 part one

---
+++ src/days/day03.rs
@@ -0,0 +1,58 @@
+use super::Day;
+use crate::util;
+
+pub fn run_day() {
+    let day: Day = Day {
+        day_num: String::from("03"),
+        part_1_test: 157,
+        part_1: 12794,
+        part_2_test: 12,
+        part_2: 14979,
+    };
+    day.run_tests(&run_parts);
+
+    fn run_parts(day: &String, part_one: bool, is_test: bool) -> i32 {
+
+        // Read file
+        let lines = util::get_file_lines(day, is_test);
+
+        let mut tot_score: i32 = 0;
+
+        for line in lines {
+            // Split into parts
+            let poc_len = line.chars().count() / 2;
+            let poc0 = &line[..poc_len];
+            let poc1 = &line[poc_len..];
+            
+            // Determine the part common to both
+            let item: char = find_common(poc0, poc1);
+            let item_val = get_item_val(item);
+            //println!("Found common {} with val {}", item, item_val);
+            tot_score = tot_score + (item_val as i32);
+        }
+        tot_score
+    }
+}
+
+fn find_common(poc0: &str, poc1: &str) -> char {
+    // Just going to brute force it here rather than use a lib
+    for find_chr in poc0.chars() {
+        for cur_chr in poc1.chars() {
+            if find_chr == cur_chr {
+                return find_chr;
+            }
+        }
+    }
+    '\0'
+}
+
+fn get_item_val(item: char) -> u32 {
+    let mut chr_to_sub = 'a';
+    let mut val_to_add = 1;
+    if (item as u32) < ('a' as u32) {
+        chr_to_sub = 'A';
+        val_to_add = 27;
+    }
+    
+    (item as u32) - (chr_to_sub as u32) + val_to_add
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,12 +1,14 @@
 mod day01;
 mod day02;
+mod day03;
 
 pub fn run_day(day: &str) -> bool {
     
     // Rust does not have reflection so do this nastiness here
     match day {
         "01" => day01::run_day(),
-        "02" => day02::run_day(),
+        //"02" => day02::run_day(),
+       // "03" => day03::run_day(),
         
         _ => return false
     }
@@ -23,18 +25,27 @@ struct Day {
 
 impl Day {
     fn run_tests(&self, part_runner: &dyn Fn(&String, bool, bool) -> i32) {
+
         //Part 1
+        print_test(true, &self.day_num, true);
         let mut ans = part_runner(&self.day_num, true, true);
+        println!("Answer for part one test: {}", ans);
         assert!(ans == self.part_1_test, "Got wrong answer.");
     
+        print_test(true, &self.day_num, false);
         ans = part_runner(&self.day_num, true, false);
+        println!("Answer for part one: {}", ans);
         assert!(ans == self.part_1, "Got wrong answer.");
     
         // Part 2
+        print_test(false, &self.day_num, true);
         ans = part_runner(&self.day_num, false, true);
+        println!("Answer for part two test: {}", ans);
         assert!(ans == self.part_2_test, "Got wrong answer.");
     
+        print_test(false, &self.day_num, false);
         ans = part_runner(&self.day_num, false, false);
+        println!("Answer for part two: {}", ans);
         assert!(ans == self.part_2, "Got wrong answer.");
 
         println!("Success!!");
