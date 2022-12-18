0967bf2da553f176a64d5b6e7ea22a1262d7d344
Scott Semyan <scottse@microsoft.com>
2022-12-17 18:12:43 -0800

    Day 10 part two

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -4,9 +4,9 @@ pub fn run_day() {
     let day: Day = Day {
         day_num: String::from("10"),
         part_1_test: String::from("13140"),
-        part_1: String::from(""),
-        part_2_test: String::from(""),
-        part_2: String::from(""), 
+        part_1: String::from("13720"),
+        part_2_test: String::from("0"),
+        part_2: String::from("0"), 
     };
     day.run_tests(&run_parts);
 
@@ -17,6 +17,8 @@ pub fn run_day() {
         let mut add_val = 0;
         let mut break_cycle = 20;
         let mut signal_str = 0;
+        let mut crt = String::new();
+        let mut crt_break = 40;
 
         // process ins
         let mut cur_line = 0;
@@ -24,8 +26,8 @@ pub fn run_day() {
         loop {
             cycle = cycle + 1;
 
-            // check for value
-            if cycle == break_cycle {
+            // check for value in part one
+            if cycle == break_cycle && part_one {
                 signal_str = signal_str + (cycle * reg);
                 println!("Cycle {} reg {} strength {}", cycle, reg, signal_str);
                 if cycle == 220 {
@@ -33,7 +35,27 @@ pub fn run_day() {
                 }
                 break_cycle = break_cycle + 40;
             }
-           
+
+            // add cr/lf every 40 cycles for part two
+            if !part_one {
+                //println!("Cycle {} reg {}", cycle, reg);
+
+                let mut pix = ".";
+                let hor_pos = cycle + 40 - crt_break - 1;
+                if hor_pos >= reg - 1 && hor_pos <= reg + 1 {
+                    pix = "#";
+                }
+                crt = crt + pix;
+                if cycle == crt_break {
+                    println!("{}", crt);
+                    crt.clear();
+                    if cycle == 240 {
+                        break;
+                    }
+                    crt_break = crt_break + 40;
+                }
+            }
+
             // Read next ins
             if !pause_ins {
                 // check for end of file
