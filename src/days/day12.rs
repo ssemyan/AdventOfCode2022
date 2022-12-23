ce3656f336d49f41b712ea4c5001ca6b20bff7dd
Scott Semyan <scottse@microsoft.com>
2022-12-23 15:48:56 -0800

    WIP

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -20,146 +20,81 @@ pub fn run_day() {
             map.push(chars);
         }
 
-        // set up hashtable so we know where we have been
-        let mut poslist: HashMap<String, i32> = HashMap::new();
+        // set up the list of points to work through
+        let mut points: Vec<PPoint> = Vec::new();
 
-        // set up global menenization (sp)
-        let mut menlist: HashMap<String, i32> = HashMap::new();
+        // Set up the list of known points
+        let mut k_points: HashMap<String, i32> = HashMap::new();
 
-        // Start wandering, end when we get to E
-        let path_cnt = find_path(&map, 0, 0, &mut poslist, b'a', -1, i32::MAX, &mut menlist);
+        // add the first point and mark as visited
+        points.push(PPoint {y: 0, x: 0, dist: 0});
+        mark_visit(0, 0, &mut k_points, 0);
 
-        path_cnt.to_string()
+        // loop until we get to the end
+        loop {
+            // the pop first point as it is the top guess
+            let p = points.remove(0);            
+
+            // get the altitude
+            let mut alt = map[p.y][p.x];
+            if alt == b'E' {
+                // at the end
+                return p.dist.to_string();
+            }
+
+            if alt == b'S' {
+                // at the start
+                alt = b'a';
+            }
+
+            // Find distance of each neighbor (will be 1) 
+
+            // N
+            if p.y > 0 {
+                try_add_point(&map, &mut points, PPoint{y: p.y - 1, x: p.x, dist: p.dist + 1}, &mut k_points, alt);
+            }
+
+            // S
+            try_add_point(&map, &mut points, PPoint{y: p.y + 1, x: p.x, dist: p.dist + 1}, &mut k_points, alt);
+
+            // W
+            if p.x > 0 {
+                try_add_point(&map, &mut points, PPoint{y: p.y, x: p.x - 1, dist: p.dist + 1}, &mut k_points, alt);
+            }
+
+            // E
+            try_add_point(&map, &mut points, PPoint{y: p.y, x: p.x + 1, dist: p.dist + 1}, &mut k_points, alt);
+        }
     }
 }
 
-// return the path lenth to end OR i32::max if invalid (using a Result or Option is a pain)
-fn find_path(
-    map: &Vec<Vec<u8>>,
-    cur_x: usize,
-    cur_y: usize,
-    poslist: &mut HashMap<String, i32>,
-    prev_alt: u8,
-    move_cnt: i32,
-    min_move_cnt: i32,
-    menlist: &mut HashMap<String, i32>,
-) -> i32 {
-    // add the move
-    let new_move_cnt = move_cnt + 1;
-
-    // If we found a shorter path abandon this path
-    if new_move_cnt > min_move_cnt {
-        return i32::MAX;
+fn try_add_point(map: &Vec<Vec<u8>>, points: &mut Vec<PPoint>, p: PPoint, k_points: &mut HashMap<String, i32>, alt: u8) {
+    if check_point(&map, p.x, p.y, &k_points, alt) {
+        mark_visit(p.x, p.y, k_points, p.dist);
+        points.insert(0, p);
     }
+}
 
-    // Check the men list if we have seen a shorter route
-    let p_visit = check_visit(cur_x, cur_y, &menlist);
-    if p_visit.is_some() && p_visit.unwrap() < &new_move_cnt {
-        // Found a shorter path on a previous visit, abandon this path
-        return i32::MAX;
-    }
+struct PPoint {
+    y: usize,
+    x: usize,
+    dist: i32
+}
 
-    // Can only go if new spot is no more than 1 char taller
-    let mut alt = map[cur_y][cur_x];
-    let at_end = alt == b'E';
-    if at_end {
-        // at the end which is an 'e'
-        alt = b'z';
-    }
-    if alt == b'S' {
-        // at the start
-        alt = b'a';
-    }
-
-    // check alt diff
-    if alt > prev_alt && (alt - prev_alt) > 1 {
-        // New pos too high
-        return i32::MAX;
-    }
-
-    // if we are done, just return
-    if at_end {
-        return new_move_cnt;
-    }
-
-    // record the visit for this route - just use a 1
-    mark_visit(cur_x, cur_y, poslist, 1);
-
-    // also mark it for future lookups
-    mark_visit(cur_x, cur_y, menlist, new_move_cnt);
-
-    // Try all directions that are not more than one above and that are unvisited
-    let mut min_path = i32::MAX; // Set the min path to the max val
-
-    // N
-    if cur_y > 0 && check_visit(cur_x, cur_y - 1, &poslist).is_none() {
-        let n_cost = find_path(
-            map,
-            cur_x,
-            cur_y - 1,
-            &mut poslist.clone(),
-            alt,
-            new_move_cnt,
-            min_move_cnt,
-            menlist,
-        );
-        if n_cost < min_path {
-            min_path = n_cost;
+fn check_point(map: &Vec<Vec<u8>>, new_x: usize, new_y: usize, menlist: &HashMap<String, i32>, cur_alt: u8) -> bool {
+    // new node must be on the map and unvisited
+    if new_y < map.len() && new_x < map[0].len() && 
+        (check_visit(new_x, new_y, menlist).is_none() ||  {
+        let mut new_alt = map[new_y][new_x];
+        if new_alt == b'E' {
+            new_alt = b'z';
+        }
+        // check alt diff - must be at most one higher
+        if new_alt <= cur_alt || (new_alt - cur_alt) == 1 {
+            return true;
         }
     }
-
-    // S
-    if cur_y < map.len() - 1 && check_visit(cur_x, cur_y + 1, &poslist).is_none() {
-        let n_cost = find_path(
-            map,
-            cur_x,
-            cur_y + 1,
-            &mut poslist.clone(),
-            alt,
-            new_move_cnt,
-            min_move_cnt,
-            menlist,
-        );
-        if n_cost < min_path {
-            min_path = n_cost;
-        }
-    }
-
-    // E
-    if cur_x < map[0].len() - 1 && check_visit(cur_x + 1, cur_y, &poslist).is_none() {
-        let n_cost = find_path(
-            map,
-            cur_x + 1,
-            cur_y,
-            &mut poslist.clone(),
-            alt,
-            new_move_cnt,
-            min_move_cnt,
-            menlist,
-        );
-        if n_cost < min_path {
-            min_path = n_cost;
-        }
-    }
-
-    // W
-    if cur_x > 0 && check_visit(cur_x - 1, cur_y, &poslist).is_none() {
-        let n_cost = find_path(
-            map,
-            cur_x - 1,
-            cur_y,
-            &mut poslist.clone(),
-            alt,
-            new_move_cnt,
-            min_move_cnt,
-            menlist,
-        );
-        if n_cost < min_path {
-            min_path = n_cost;
-        }
-    }
-
-    min_path
+    false
 }
 
 fn mark_visit(cur_x: usize, cur_y: usize, poslist: &mut HashMap<String, i32>, val: i32) {
